5c0eab95f728b92480b1ac132195dec72c842159
Frederik Hahne <frederik.hahne@googlemail.com>
2013-03-15 09:06:12 +0100

    Fix #6
    Using Ajax Link for Submitting the new member form.
    Added Feedbackmessages.

--- application/src/main/java/de/atomfrede/forest/alumni/application/wicket/member/detail/MembersDetailForm.java
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/member/detail/MembersDetailForm.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.extensions.markup.html.form.select.Select;
@@ -29,9 +30,11 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.util.time.Duration;
 import org.apache.wicket.validation.validator.EmailAddressValidator;
 
+import de.agilecoders.wicket.markup.html.bootstrap.common.NotificationMessage;
 import de.agilecoders.wicket.markup.html.bootstrap.common.NotificationPanel;
 import de.agilecoders.wicket.markup.html.bootstrap.extensions.form.DateTextField;
 import de.agilecoders.wicket.markup.html.bootstrap.extensions.form.DateTextFieldConfig;
+import de.agilecoders.wicket.markup.html.bootstrap.form.BootstrapForm;
 import de.atomfrede.forest.alumni.application.wicket.activity.ActivityProvider;
 import de.atomfrede.forest.alumni.application.wicket.custom.DegreeSelectOption;
 import de.atomfrede.forest.alumni.application.wicket.custom.SectorSelectOption;
@@ -50,67 +53,92 @@ import de.atomfrede.forest.alumni.domain.entity.sector.Sector;
 import de.atomfrede.forest.alumni.service.member.MemberService;
 
 @SuppressWarnings("serial")
-public class MembersDetailForm extends Form<Member>{
+public class MembersDetailForm extends BootstrapForm<Member> {
 
 	@SpringBean
 	DegreeDao degreeDao;
-	
+
 	@SpringBean
 	SectorDao sectorDao;
-	
+
 	@SpringBean
 	CompanyDao companyDao;
-	
+
 	@SpringBean
 	ActivityDao activityDao;
-	
+
 	@SpringBean
 	DepartmentDao departmentDao;
-	
+
 	@SpringBean
 	MemberService memberService;
-	
+
 	Type editType;
 	Degree selectedDegree;
 	Sector selectedSector;
-	
+
 	NotificationPanel feedbackPanel;
-	
+
 	WebMarkupContainer firstnameWrapper, lastnameWrapper, personalMailWrapper;
-	
+
 	RequiredTextField<String> firstname, lastname, personalMail;
-	TextField<String> personalAddon, profession, salutation, title, graduationYear, personalStreet, personalTown, personalPostcode, workMail, personalMobile, personalFax, personalPhone, personalInternet, workPhone, workMobile, workFax, workInternet;
+	TextField<String> personalAddon, profession, salutation, title,
+			graduationYear, personalStreet, personalTown, personalPostcode,
+			workMail, personalMobile, personalFax, personalPhone,
+			personalInternet, workPhone, workMobile, workFax, workInternet;
 	DateTextField entryDate;
-	
-	
+
 	DataView<Activity> activities;
-	
-	//Now the data for this formfields. To reuse this panel for new (=non existing entities we don't use the model directly...)
+
+	// Now the data for this formfields. To reuse this panel for new (=non
+	// existing entities we don't use the model directly...)
 	String _salutation, _title, _firstname, _lastname;
 	Date _entryDate;
-	String _personalStreet, _personalNumber, _personalAddon, _personalPostcode, _personalTown, _personalMail, _personalMobile, _personalPhone, _personalFax, _personalInternet;
+	String _personalStreet, _personalNumber, _personalAddon, _personalPostcode,
+			_personalTown, _personalMail, _personalMobile, _personalPhone,
+			_personalFax, _personalInternet;
 	String _graduationYear, _profession;
 	String _workMail, _workFax, _workMobile, _workInternet, _workPhone;
-	
+
 	Map<String, Boolean> activityName_checked = new HashMap<>();
-	
-	public MembersDetailForm(String id, Type editType, AbstractEntityModel<Member> model) {
+
+	public MembersDetailForm(String id, Type editType,
+			AbstractEntityModel<Member> model) {
 		super(id, model);
 		this.editType = editType;
-		
+
 		feedbackPanel = new NotificationPanel("feedbackPanel");
+		feedbackPanel.setOutputMarkupId(true);
 		add(feedbackPanel);
+
+		AjaxButton submitBtn = new AjaxButton("submit-btn", this) {
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+				// repaint the feedback panel so that it is hidden
+				target.add(feedbackPanel);
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target, Form<?> form) {
+				target.add(feedbackPanel);
+				target.add(firstnameWrapper);
+				target.add(lastnameWrapper);
+				target.add(personalMailWrapper);
+			}
+		};
+		
+		add(submitBtn);
 		
 		initFormValues(model);
-		
+
 		setupPersonalTab();
 		setupWorkTab();
 		setupDegreeTab();
 		setupActivityTab();
 	}
-	
-	private void initFormValues(AbstractEntityModel<Member> model){
-		if(editType == Type.Create){
+
+	private void initFormValues(AbstractEntityModel<Member> model) {
+		if (editType == Type.Create) {
 			_salutation = "";
 			_title = "";
 			_firstname = "";
@@ -124,10 +152,10 @@ public class MembersDetailForm extends Form<Member>{
 			_personalPostcode = "";
 			_personalStreet = "";
 			_personalTown = "";
-			
+
 			_entryDate = new Date();
-			
-		}else if(model.getObject() != null){
+
+		} else if (model.getObject() != null) {
 			Member mem = model.getObject();
 			_graduationYear = mem.getYearOfGraduation();
 			_salutation = mem.getSalutation();
@@ -148,93 +176,121 @@ public class MembersDetailForm extends Form<Member>{
 			_workMail = mem.getContactData().getEmailD();
 			_workMobile = mem.getContactData().getMobileD();
 			_workPhone = mem.getContactData().getPhoneD();
-			
+
 			_entryDate = mem.getEntryDate();
 			_profession = mem.getProfession();
-			
+
 			selectedDegree = mem.getDegree();
 			selectedSector = mem.getSector();
-			
-			for(Activity act:activityDao.findAll()){
-				if(mem.getActivities().contains(act)){
+
+			for (Activity act : activityDao.findAll()) {
+				if (mem.getActivities().contains(act)) {
 					activityName_checked.put(act.getActivity(), Boolean.TRUE);
-				}else{
+				} else {
 					activityName_checked.put(act.getActivity(), Boolean.FALSE);
 				}
 			}
 		}
 	}
+
 	/**
 	 * Sets up the fourth ('activities') tab
 	 */
-	private void setupActivityTab(){
+	private void setupActivityTab() {
 		populateItems();
 	}
-	
+
 	/**
 	 * Sets up the third ('work') tab
 	 */
-	private void setupWorkTab(){
-		workMail = new TextField<String>("work.mail", new PropertyModel<String>(this, "_workMail"));
+	private void setupWorkTab() {
+		workMail = new TextField<String>("work.mail",
+				new PropertyModel<String>(this, "_workMail"));
 		workMail.add(EmailAddressValidator.getInstance());
-		workFax = new TextField<String>("work.fax", new PropertyModel<String>(this, "_workFax"));
-		workInternet = new TextField<String>("work.internet", new PropertyModel<String>(this, "_workInternet"));
-		workMobile = new TextField<String>("work.mobile", new PropertyModel<String>(this, "_workMobile"));
-		workPhone = new TextField<String>("work.phone", new PropertyModel<String>(this, "_workPhone"));
-		
+		workFax = new TextField<String>("work.fax", new PropertyModel<String>(
+				this, "_workFax"));
+		workInternet = new TextField<String>("work.internet",
+				new PropertyModel<String>(this, "_workInternet"));
+		workMobile = new TextField<String>("work.mobile",
+				new PropertyModel<String>(this, "_workMobile"));
+		workPhone = new TextField<String>("work.phone",
+				new PropertyModel<String>(this, "_workPhone"));
+
 		add(workMail);
 		add(workFax);
 		add(workInternet);
 		add(workMobile);
 		add(workPhone);
-		
-		//Branche
+
+		// Branche
 		List<Sector> sectors = sectorDao.findAll();
-				
-		Select<Sector> sectorSelect = new Select<Sector>("sector-select", new PropertyModel<Sector>(this, "selectedSector"));
-				
+
+		Select<Sector> sectorSelect = new Select<Sector>("sector-select",
+				new PropertyModel<Sector>(this, "selectedSector"));
+
 		sectorSelect.add(new ListView<Sector>("sector-options", sectors) {
 
 			@Override
 			protected void populateItem(ListItem<Sector> item) {
-				item.add(new SectorSelectOption("sector-option", item.getModelObject()));
+				item.add(new SectorSelectOption("sector-option", item
+						.getModelObject()));
 			}
 		});
-				
+
 		add(sectorSelect);
 	}
-	
+
 	/**
 	 * Sets up the first ('personal') tab.
 	 */
-	private void setupPersonalTab(){
-		firstnameWrapper = new WebMarkupContainer("member.detail.firstname.wrapper");
-		lastnameWrapper = new WebMarkupContainer("member.detail.lastname.wrapper");
-		personalMailWrapper = new WebMarkupContainer("member.detail.personal.mail.wrapper");
+	private void setupPersonalTab() {
+		firstnameWrapper = new WebMarkupContainer(
+				"member.detail.firstname.wrapper");
+		lastnameWrapper = new WebMarkupContainer(
+				"member.detail.lastname.wrapper");
+		personalMailWrapper = new WebMarkupContainer(
+				"member.detail.personal.mail.wrapper");
+
+		firstnameWrapper.setOutputMarkupId(true);
+		lastnameWrapper.setOutputMarkupId(true);
+		personalMailWrapper.setOutputMarkupId(true);
 		
 		add(firstnameWrapper);
 		add(lastnameWrapper);
 		add(personalMailWrapper);
-		
-		salutation = new TextField<String>("salutation", new PropertyModel<String>(this, "_salutation"));
-		title = new TextField<String>("title", new PropertyModel<String>(this, "_title"));
-		firstname = new RequiredTextField<String>("firstname", new PropertyModel<String>(this, "_firstname"));
-		lastname = new RequiredTextField<String>("lastname", new PropertyModel<String>(this, "_lastname"));
-		
+
+		salutation = new TextField<String>("salutation",
+				new PropertyModel<String>(this, "_salutation"));
+		title = new TextField<String>("title", new PropertyModel<String>(this,
+				"_title"));
+		firstname = new RequiredTextField<String>("firstname",
+				new PropertyModel<String>(this, "_firstname"));
+		lastname = new RequiredTextField<String>("lastname",
+				new PropertyModel<String>(this, "_lastname"));
+
 		firstnameWrapper.add(firstname);
 		lastnameWrapper.add(lastname);
-		
-		personalAddon = new TextField<String>("personal.addon", new PropertyModel<String>(this, "_personalAddon"));
-		personalFax = new TextField<String>("personal.fax", new PropertyModel<String>(this, "_personalFax"));
-		personalMobile = new TextField<String>("personal.mobile", new PropertyModel<String>(this, "_personalMobile"));
-		personalPhone = new TextField<String>("personal.phone", new PropertyModel<String>(this, "_personalPhone"));
-		personalInternet = new TextField<String>("personal.internet", new PropertyModel<String>(this, "_personalInternet"));
-		personalPostcode = new TextField<String>("personal.postcode", new PropertyModel<String>(this, "_personalPostcode"));
-		personalStreet = new TextField<String>("personal.street", new PropertyModel<String>(this, "_personalStreet"));
-		personalTown = new TextField<String>("personal.town", new PropertyModel<String>(this, "_personalTown"));
-		personalMail = new RequiredTextField<String>("personal.mail", new PropertyModel<String>(this, "_personalMail"));
+
+		personalAddon = new TextField<String>("personal.addon",
+				new PropertyModel<String>(this, "_personalAddon"));
+		personalFax = new TextField<String>("personal.fax",
+				new PropertyModel<String>(this, "_personalFax"));
+		personalMobile = new TextField<String>("personal.mobile",
+				new PropertyModel<String>(this, "_personalMobile"));
+		personalPhone = new TextField<String>("personal.phone",
+				new PropertyModel<String>(this, "_personalPhone"));
+		personalInternet = new TextField<String>("personal.internet",
+				new PropertyModel<String>(this, "_personalInternet"));
+		personalPostcode = new TextField<String>("personal.postcode",
+				new PropertyModel<String>(this, "_personalPostcode"));
+		personalStreet = new TextField<String>("personal.street",
+				new PropertyModel<String>(this, "_personalStreet"));
+		personalTown = new TextField<String>("personal.town",
+				new PropertyModel<String>(this, "_personalTown"));
+		personalMail = new RequiredTextField<String>("personal.mail",
+				new PropertyModel<String>(this, "_personalMail"));
 		personalMail.add(EmailAddressValidator.getInstance());
-		
+
 		add(personalAddon);
 		add(personalPostcode);
 		add(personalStreet);
@@ -244,122 +300,142 @@ public class MembersDetailForm extends Form<Member>{
 		add(personalMobile);
 		add(personalPhone);
 		add(personalInternet);
-		
+
 		add(salutation);
 		add(title);
-		
+
 		DateTextFieldConfig conf = new DateTextFieldConfig();
 		conf.withView(DateTextFieldConfig.View.Year);
 		conf.autoClose(true);
-		entryDate = new DateTextField("entrydate", new PropertyModel<Date>(this, "_entryDate"), conf);
-		
+		entryDate = new DateTextField("entrydate", new PropertyModel<Date>(
+				this, "_entryDate"), conf);
+
 		add(entryDate);
 	}
-	
+
 	/**
 	 * Sets up the second ('degree') tab
 	 */
-	private void setupDegreeTab(){
-		profession = new TextField<String>("profession", new PropertyModel<String>(this, "_profession"));
-		graduationYear = new TextField<String>("graduationyear", new PropertyModel<String>(this, "_graduationYear"));
-		
+	private void setupDegreeTab() {
+		profession = new TextField<String>("profession",
+				new PropertyModel<String>(this, "_profession"));
+		graduationYear = new TextField<String>("graduationyear",
+				new PropertyModel<String>(this, "_graduationYear"));
+
 		List<Degree> degrees = degreeDao.findAll();
-		
-		Select<Degree> degreeSelect = new Select<Degree>("degree-select", new PropertyModel<Degree>(this, "selectedDegree"));
-		
+
+		Select<Degree> degreeSelect = new Select<Degree>("degree-select",
+				new PropertyModel<Degree>(this, "selectedDegree"));
+
 		degreeSelect.add(new ListView<Degree>("degree-options", degrees) {
 
 			@Override
 			protected void populateItem(ListItem<Degree> item) {
-				item.add(new DegreeSelectOption("degree-option", item.getModelObject()));
+				item.add(new DegreeSelectOption("degree-option", item
+						.getModelObject()));
 			}
 		});
-		
+
 		add(profession);
 		add(graduationYear);
 		add(degreeSelect);
 	}
-	
+
 	/**
 	 * Populates the last Tab with checkboxes for possible activites.
 	 */
-	private void populateItems(){
-		activities = new DataView<Activity>("activities", new ActivityProvider()) {
+	private void populateItems() {
+		activities = new DataView<Activity>("activities",
+				new ActivityProvider()) {
 
 			@Override
 			protected void populateItem(Item<Activity> item) {
-				//TODO check which must be the default value...
-				CheckBox checkBox =null;
+				// TODO check which must be the default value...
+				CheckBox checkBox = null;
 				boolean checked = false;
 				switch (editType) {
 				case Create:
 					checkBox = new CheckBox("activity", Model.of(Boolean.FALSE));
 					break;
 				case Edit:
-					checked = activityName_checked.get(item.getModelObject().getActivity());
+					checked = activityName_checked.get(item.getModelObject()
+							.getActivity());
 					checkBox = new CheckBox("activity", Model.of(checked));
 					break;
 				case Show:
-					checked = activityName_checked.get(item.getModelObject().getActivity());
+					checked = activityName_checked.get(item.getModelObject()
+							.getActivity());
 					checkBox = new CheckBox("activity", Model.of(Boolean.FALSE));
 					break;
 				default:
 					break;
 				}
-				
-				checkBox.add(new AttributeModifier("data-label", item.getModelObject().getActivity()));
+
+				checkBox.add(new AttributeModifier("data-label", item
+						.getModelObject().getActivity()));
 				item.add(checkBox);
 			}
 		};
-		
+
 		add(activities);
-		
+
 	}
-	
+
 	@Override
 	public void renderHead(IHeaderResponse response) {
 		super.renderHead(response);
-		response.render(OnDomReadyHeaderItem.forScript("$('.selectpicker').selectpicker();"));
-		response.render(OnDomReadyHeaderItem.forScript("$('input.checkboxes-pretty').prettyCheckable({color: 'red'});"));
-		
+		response.render(OnDomReadyHeaderItem
+				.forScript("$('.selectpicker').selectpicker();"));
+		response.render(OnDomReadyHeaderItem
+				.forScript("$('input.checkboxes-pretty').prettyCheckable({color: 'red'});"));
+
 	}
-	
+
 	@Override
 	protected void onError() {
 		// Only on validation errors we make the feedbackpanel visible
 		this.feedbackPanel.setVisible(true);
 		this.feedbackPanel.hideAfter(Duration.seconds(10));
-		if(!firstname.isValid()){
+		if (!firstname.isValid()) {
 			firstnameWrapper.add(new AttributeAppender("class", " error"));
-		}else{
-			firstnameWrapper.add(new AttributeModifier("class", "control-group"));
+		} else {
+			firstnameWrapper
+					.add(new AttributeModifier("class", "control-group"));
 		}
-		if(!lastname.isValid()){
+		if (!lastname.isValid()) {
 			lastnameWrapper.add(new AttributeAppender("class", " error"));
-		}else{
-			lastnameWrapper.add(new AttributeModifier("class", "control-group"));
+		} else {
+			lastnameWrapper
+					.add(new AttributeModifier("class", "control-group"));
 		}
-		if(!personalMail.isValid()){
+		if (!personalMail.isValid()) {
 			personalMailWrapper.add(new AttributeAppender("class", " error"));
-		}else{
-			personalMailWrapper.add(new AttributeModifier("class", "control-group"));
+		} else {
+			personalMailWrapper.add(new AttributeModifier("class",
+					"control-group"));
 		}
 	}
-	
+
 	@Override
 	public void onSubmit() {
 		Member member = null;
-		if(editType == Type.Create){
-			member = memberService.createMember(_firstname, _lastname, _personalMail);
+		if (editType == Type.Create) {
+			member = memberService.createMember(_firstname, _lastname,
+					_personalMail);
 			editType = Type.Create;
-			if(getPage() instanceof MemberDetailPageListener){
-				((MemberDetailPageListener)getPage()).editTypeChanged(editType);
+			if (getPage() instanceof MemberDetailPageListener) {
+				((MemberDetailPageListener) getPage())
+						.editTypeChanged(editType);
 			}
-		}else{
+		} else {
 			member = getModelObject();
 		}
 		
-		
+		//It Was succesfull, so display a notifications about this
+		NotificationMessage nf = new NotificationMessage(Model.of("Gespeichert"));
+		nf.hideAfter(Duration.seconds(3));
+		success(nf);
+
 	}
 
 }
