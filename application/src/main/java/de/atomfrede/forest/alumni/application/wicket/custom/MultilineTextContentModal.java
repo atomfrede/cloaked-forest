48ca01f7684e8f5f87ad3ebef614e0a0bcf34bcd
Frederik Hahne <frederik.hahne@googlemail.com>
2013-04-01 11:47:25 +0200

    Multi line text content modal compoenent.

---
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/custom/MultilineTextContentModal.java
@@ -0,0 +1,25 @@
+package de.atomfrede.forest.alumni.application.wicket.custom;
+
+import org.apache.wicket.markup.html.basic.MultiLineLabel;
+import org.apache.wicket.model.IModel;
+
+import de.agilecoders.wicket.markup.html.bootstrap.dialog.Modal;
+
+@SuppressWarnings("serial")
+/**
+ * A special text content for modal dialogs tha uses a multi line label and does not escape markup strings. So 
+ * line breaks can be used inside a modal dialog generated dynamically.
+ * @author fred
+ *
+ */
+public class MultilineTextContentModal extends Modal {
+
+	public MultilineTextContentModal(String markupId, IModel<String> model) {
+		super(markupId);
+		setEscapeModelStrings(false);
+		MultiLineLabel label = new MultiLineLabel("content", model);
+		label.setEscapeModelStrings(false);
+		add(label);
+	}
+
+}
