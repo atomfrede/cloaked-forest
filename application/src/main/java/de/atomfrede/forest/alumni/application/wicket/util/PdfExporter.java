262c23d6e1d5b7d5907579dff37655b3adf2d622
Frederik Hahne <atomfrede@gmail.com>
2013-04-09 15:27:17 +0200

    Update #18

--- application/src/main/java/de/atomfrede/forest/alumni/application/wicket/util/PdfExporter.java
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/util/PdfExporter.java
@@ -6,8 +6,10 @@ import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.HtmlUtils;
 import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import de.atomfrede.forest.alumni.domain.dao.member.MemberDao;
@@ -28,23 +30,31 @@ public class PdfExporter {
 
 	/**
 	 * Returns header and required CSS styles.
+	 * 
 	 * @return
 	 */
 	private String getHeaderAndStyles() {
 		StringBuilder headerAndStyles = new StringBuilder(
 				"<!DOCTYPE html><html><head>");
-		
+
 		SimpleDateFormat dateFormatter = new SimpleDateFormat("dd.MM.yyyy");
 		String date = dateFormatter.format(new Date());
-		headerAndStyles.append("<style type=\"text/css\">@page {@bottom-right { content:\"Seite \" counter(page) \" von \" counter(pages); }}  ");
-		headerAndStyles.append("@page {@bottom-center {content:\"Abrufdatum: "+date+"\"}}");
+		headerAndStyles
+				.append("<style type=\"text/css\">@page {@bottom-right { content:\"Seite \" counter(page) \" von \" counter(pages); }}  ");
+		headerAndStyles.append("@page {@bottom-center {content:\"Abrufdatum: "
+				+ date + "\"}}");
 		headerAndStyles.append(".header{width: 100%; padding: 5px;}");
-		headerAndStyles.append(".name{width: 40%; align:left; text-align: left;}");
-		headerAndStyles.append(".degree{width: 40%; align:right; text-align: right;}");
+		headerAndStyles
+				.append(".name{width: 40%; align:left; text-align: left;}");
+		headerAndStyles
+				.append(".degree{width: 40%; align:right; text-align: right;}");
 		headerAndStyles.append(".address{width: 100%; }");
-		headerAndStyles.append(".address-header{ width: 100%; padding: 5px; border-bottom: 1px solid #DDDDDD; font-weight: bold;: bold;}");
-		headerAndStyles.append(".work-address-header{ width: 100%; padding: 5px; border-bottom: 1px solid #DDDDDD; font-weight: bold;: bold;}");
-		headerAndStyles.append("body { font-family: 'Ubuntu',Tahoma,sans-serif;}");
+		headerAndStyles
+				.append(".address-header{ width: 100%; padding: 5px; border-bottom: 1px solid #DDDDDD; font-weight: bold;: bold;}");
+		headerAndStyles
+				.append(".work-address-header{ width: 100%; padding: 5px; border-bottom: 1px solid #DDDDDD; font-weight: bold;: bold;}");
+		headerAndStyles
+				.append("body { font-family: 'Ubuntu',Tahoma,sans-serif;}");
 		headerAndStyles.append("</style>");
 		headerAndStyles.append("</head>");
 		headerAndStyles.append("<body>");
@@ -68,8 +78,7 @@ public class PdfExporter {
 		}
 		if (member.getProfession() != null
 				&& StringCheckUtil.isStringSet(member.getProfession())) {
-			// TODO encode as HTML
-			// sb.append(member.getProfession()+" ");
+			sb.append(member.getProfession().replaceAll("&", "&amp;") + " ");
 		}
 		if (member.getYearOfGraduation() != null
 				&& StringCheckUtil.isStringSet(member.getYearOfGraduation())) {
@@ -99,101 +108,114 @@ public class PdfExporter {
 
 	private String getPrivateAddress(Member member) {
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("<div class=\"address-left\">");
-		
-		if(member.getContactData() != null){
+
+		if (member.getContactData() != null) {
 			ContactData cData = member.getContactData();
 
-			if(member.getSalutation() != null && StringCheckUtil.isStringSet(member.getSalutation())){
+			if (member.getSalutation() != null
+					&& StringCheckUtil.isStringSet(member.getSalutation())) {
 				sb.append(member.getSalutation());
 			}
-			
+
 			sb.append("<br/>");
-			
-			if(member.getDegree() != null && StringCheckUtil.isStringSet(member.getDegree().getShortForm())){
-				sb.append(member.getDegree().getShortForm()+" ");
-				sb.append(member.getFirstname()+" "+member.getLastname()+" ");
+
+			if (member.getDegree() != null
+					&& StringCheckUtil.isStringSet(member.getDegree()
+							.getShortForm())) {
+				sb.append(member.getDegree().getShortForm() + " ");
+				sb.append(member.getFirstname() + " " + member.getLastname()
+						+ " ");
 			}
-			
+
 			sb.append("<br/>");
-			if(cData.getStreet() != null && StringCheckUtil.isStringSet(cData.getStreet())){
-				sb.append(cData.getStreet()+" ");
-				if(cData.getNumber() != null && StringCheckUtil.isStringSet(cData.getNumber())){
+			if (cData.getStreet() != null
+					&& StringCheckUtil.isStringSet(cData.getStreet())) {
+				sb.append(cData.getStreet() + " ");
+				if (cData.getNumber() != null
+						&& StringCheckUtil.isStringSet(cData.getNumber())) {
 					sb.append(cData.getNumber());
 				}
 			}
-			
+
 			sb.append("<br/>");
-			
-			if(cData.getPostCode() != null && StringCheckUtil.isStringSet(cData.getPostCode())){
-				sb.append(cData.getPostCode()+" ");
-				if(cData.getTown() != null && StringCheckUtil.isStringSet(cData.getTown())){
+
+			if (cData.getPostCode() != null
+					&& StringCheckUtil.isStringSet(cData.getPostCode())) {
+				sb.append(cData.getPostCode() + " ");
+				if (cData.getTown() != null
+						&& StringCheckUtil.isStringSet(cData.getTown())) {
 					sb.append(cData.getTown());
 				}
 			}
-			
+
 			sb.append("<br/>");
-			
-			if(cData.getCountry() != null && StringCheckUtil.isStringSet(cData.getCountry())){
+
+			if (cData.getCountry() != null
+					&& StringCheckUtil.isStringSet(cData.getCountry())) {
 				sb.append(cData.getCountry());
 			}
 			sb.append("<br/>");
-		
-		
+
 			sb.append("</div>");
-			
+
 			sb.append("<div class=\"address-right\">");
-			
+
 			sb.append("<table>");
-			
+
 			sb.append("<tr>");
 			sb.append("<td>Tel: </td>");
 			sb.append("<td>");
-			if(cData.getPhone() != null && StringCheckUtil.isStringSet(cData.getPhone())){
+			if (cData.getPhone() != null
+					&& StringCheckUtil.isStringSet(cData.getPhone())) {
 				sb.append(cData.getPhone());
 			}
 			sb.append("</td>");
 			sb.append("</tr>");
-			
+
 			sb.append("<tr>");
 			sb.append("<td>Fax: </td>");
 			sb.append("<td>");
-			if(cData.getFax() != null && StringCheckUtil.isStringSet(cData.getFax())){
+			if (cData.getFax() != null
+					&& StringCheckUtil.isStringSet(cData.getFax())) {
 				sb.append(cData.getFax());
 			}
 			sb.append("</td>");
 			sb.append("</tr>");
-			
+
 			sb.append("<tr>");
 			sb.append("<td>Mobil: </td>");
 			sb.append("<td>");
-			if(cData.getMobile() != null && StringCheckUtil.isStringSet(cData.getMobile())){
+			if (cData.getMobile() != null
+					&& StringCheckUtil.isStringSet(cData.getMobile())) {
 				sb.append(cData.getMobile());
 			}
 			sb.append("</td>");
 			sb.append("</tr>");
-			
+
 			sb.append("<tr>");
 			sb.append("<td>eMail: </td>");
 			sb.append("<td>");
-			if(cData.getEmail() != null && StringCheckUtil.isStringSet(cData.getEmail())){
+			if (cData.getEmail() != null
+					&& StringCheckUtil.isStringSet(cData.getEmail())) {
 				sb.append(cData.getEmail());
 			}
 			sb.append("</td>");
 			sb.append("</tr>");
-			
+
 			sb.append("<tr>");
 			sb.append("<td>Internet: </td>");
 			sb.append("<td>");
-			if(cData.getInternet() != null && StringCheckUtil.isStringSet(cData.getInternet())){
+			if (cData.getInternet() != null
+					&& StringCheckUtil.isStringSet(cData.getInternet())) {
 				sb.append(cData.getInternet());
 			}
 			sb.append("</td>");
 			sb.append("</tr>");
-			
+
 			sb.append("</table>");
-			
+
 		}
 		sb.append("</div>");
 		return sb.toString();
@@ -221,7 +243,7 @@ public class PdfExporter {
 			if (member.getActivities() != null
 					&& !member.getActivities().isEmpty()) {
 				for (Activity act : member.getActivities()) {
-					// sb.append(act+" ");
+					sb.append(act.getActivity().replaceAll("&", "&amp;") + " ");
 				}
 			}
 
@@ -251,6 +273,9 @@ public class PdfExporter {
 
 			content.append("</body></html>");
 
+			// String escapedHtml =
+			// StringEscapeUtils.escapeHtml4(content.toString());
+//			String escapedHtml = HtmlUtils.htmlEscape(content.toString());
 			ITextRenderer renderer = new ITextRenderer();
 			renderer.setDocumentFromString(content.toString());
 			renderer.layout();
