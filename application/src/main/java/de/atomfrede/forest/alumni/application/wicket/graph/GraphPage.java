c5f5b492e30a39006f0740df3fcf739a78049d6d
Frederik Hahne <frederik.hahne@googlemail.com>
2013-04-12 09:24:36 +0200

    Pages have bookmarable links and mounting.

--- application/src/main/java/de/atomfrede/forest/alumni/application/wicket/graph/GraphPage.java
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/graph/GraphPage.java
@@ -1,8 +1,11 @@
 package de.atomfrede.forest.alumni.application.wicket.graph;
 
+import org.wicketstuff.annotation.mount.MountPath;
+
 import de.atomfrede.forest.alumni.application.wicket.base.BasePage;
 
 @SuppressWarnings("serial")
+@MountPath(value = "/graphs", alt = "/graphs")
 public class GraphPage extends BasePage<Void> {
 
 	public GraphPage() {
--- application/src/main/java/de/atomfrede/forest/alumni/application/wicket/query/QueryPage.java
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/query/QueryPage.java
@@ -1,8 +1,11 @@
 package de.atomfrede.forest.alumni.application.wicket.query;
 
+import org.wicketstuff.annotation.mount.MountPath;
+
 import de.atomfrede.forest.alumni.application.wicket.base.BasePage;
 
 @SuppressWarnings("serial")
+@MountPath(value = "/queries", alt = "/queries")
 public class QueryPage extends BasePage<Void> {
 
 	public QueryPage() {
