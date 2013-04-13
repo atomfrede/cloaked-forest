8bbb608bf69693950eebcf0575d65a745bea3816
Frederik Hahne <frederik.hahne@googlemail.com>
2013-04-13 11:56:54 +0200

    Checkstyle.

--- service/src/main/java/de/atomfrede/forest/alumni/service/user/UserService.java
+++ service/src/main/java/de/atomfrede/forest/alumni/service/user/UserService.java
@@ -3,10 +3,28 @@ package de.atomfrede.forest.alumni.service.user;
 import de.atomfrede.forest.alumni.domain.entity.user.User;
 import de.atomfrede.forest.alumni.service.EntityService;
 
+/**
+ * Service methods interacting with a user object.
+ * 
+ * @author fred
+ *
+ */
 public interface UserService extends EntityService<User> {
 
+	/**
+	 * Deletes the user with the given id.
+	 * 
+	 * @param id
+	 * @return
+	 */
 	boolean deleteUser(long id);
-	
+
+	/**
+	 * Returns the user with the given username.
+	 * 
+	 * @param username
+	 * @return
+	 */
 	User getByUsername(String username);
 
 	/**
@@ -21,9 +39,31 @@ public interface UserService extends EntityService<User> {
 	 */
 	User createUser(String username, String firstname, String lastname,
 			String email, String password) throws UsernameAlreadyTakenException;
-	
-	User createUser(String username, String firstname, String lastname,String password) throws UsernameAlreadyTakenException;
 
+	/**
+	 * Creates a user with the given parameters.
+	 * 
+	 * The email is set to the empty string.
+	 * 
+	 * @param username
+	 * @param firstname
+	 * @param lastname
+	 * @param password
+	 * @return
+	 * @throws UsernameAlreadyTakenException
+	 */
+	User createUser(String username, String firstname, String lastname,
+			String password) throws UsernameAlreadyTakenException;
+
+	/**
+	 * Checks if a user with the given username can be created.
+	 * 
+	 * In particular, this method returns true iff there is no user already with
+	 * the given username.
+	 * 
+	 * @param username
+	 * @return
+	 */
 	boolean canCreateUser(String username);
 
 }
