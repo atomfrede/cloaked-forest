aca1cd3c820108c67208c8c2caa3e9de26a14c97
Frederik Hahne <atomfrede@gmail.com>
2013-04-30 12:07:21 +0200

    Simple Sector Listing.

---
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/company/detail/CompanyDetailPage.java
@@ -0,0 +1,8 @@
+package de.atomfrede.forest.alumni.application.wicket.company.detail;
+
+import de.atomfrede.forest.alumni.application.wicket.base.BasePage;
+
+@SuppressWarnings("serial")
+public class CompanyDetailPage extends BasePage<Void> {
+
+}
---
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/sector/SectorProvider.java
@@ -0,0 +1,45 @@
+package de.atomfrede.forest.alumni.application.wicket.sector;
+
+import java.util.Iterator;
+
+import org.apache.wicket.injection.Injector;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import de.atomfrede.forest.alumni.application.wicket.model.AbstractEntityModel;
+import de.atomfrede.forest.alumni.domain.dao.sector.SectorDao;
+import de.atomfrede.forest.alumni.domain.entity.sector.Sector;
+
+@SuppressWarnings("serial")
+public class SectorProvider implements IDataProvider<Sector>{
+
+	@SpringBean
+	private SectorDao sectorDao;
+
+	public SectorProvider() {
+		Injector.get().inject(this);
+	}
+	
+	@Override
+	public void detach() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Iterator<? extends Sector> iterator(long offset, long count) {
+		return sectorDao.list(offset, count, "sector", false).iterator();
+	}
+
+	@Override
+	public IModel<Sector> model(Sector object) {
+		return new AbstractEntityModel<Sector>(object);
+	}
+
+	@Override
+	public long size() {
+		return sectorDao.count();
+	}
+
+}
