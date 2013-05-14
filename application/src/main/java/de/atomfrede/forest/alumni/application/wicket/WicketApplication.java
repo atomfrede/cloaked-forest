8604b0a8e6fc2c96ca8c7d17761248c46b223f57
Frederik Hahne <frederik.hahne@googlemail.com>
2013-05-14 18:47:31 +0200

    Deployment Configuration

--- application/src/main/java/de/atomfrede/forest/alumni/application/wicket/WicketApplication.java
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/WicketApplication.java
@@ -100,7 +100,7 @@ public class WicketApplication extends WebApplication implements
 
 	@Override
 	public RuntimeConfigurationType getConfigurationType() {
-		return RuntimeConfigurationType.DEVELOPMENT;
+		return RuntimeConfigurationType.DEPLOYMENT;
 	}
 
 }
