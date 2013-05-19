d37f55bac98630333c54b192a58b8959996097b6
Frederik Hahne <frederik.hahne@googlemail.com>
2013-05-19 11:33:37 +0200

    Javadoc.

--- domain/src/main/java/de/atomfrede/forest/alumni/domain/MySQL5InnoDBUtf8Dialect.java
+++ domain/src/main/java/de/atomfrede/forest/alumni/domain/MySQL5InnoDBUtf8Dialect.java
@@ -2,8 +2,16 @@ package de.atomfrede.forest.alumni.domain;
 
 import org.hibernate.dialect.MySQL5InnoDBDialect;
 
+/**
+ * Simple custom Inno Database Dialect using default UTF-8 Charset encoding.
+ * 
+ * @see MySQL5InnoDBDialect
+ * @author fred
+ *
+ */
 public class MySQL5InnoDBUtf8Dialect extends MySQL5InnoDBDialect {
 
+	@Override
 	public String getTableTypeString() {
 		return " ENGINE=InnoDB DEFAULT CHARSET=utf8";
 	}
