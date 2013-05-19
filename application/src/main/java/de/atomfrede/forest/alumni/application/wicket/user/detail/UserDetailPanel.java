fbcf7f00b50d033a90d8a2dd992ac1ac125bb402
Frederik Hahne <frederik.hahne@googlemail.com>
2013-05-19 13:27:18 +0200

    PMD Checks.

--- application/src/main/java/de/atomfrede/forest/alumni/application/wicket/user/detail/UserDetailPanel.java
+++ application/src/main/java/de/atomfrede/forest/alumni/application/wicket/user/detail/UserDetailPanel.java
@@ -1,18 +1,13 @@
 package de.atomfrede.forest.alumni.application.wicket.user.detail;
 
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.spring.injection.annot.SpringBean;
 
 import de.atomfrede.forest.alumni.application.wicket.model.AbstractEntityModel;
 import de.atomfrede.forest.alumni.application.wicket.user.detail.UserDetailPage.Type;
-import de.atomfrede.forest.alumni.domain.dao.user.UserDao;
 import de.atomfrede.forest.alumni.domain.entity.user.User;
 
 @SuppressWarnings("serial")
 public class UserDetailPanel extends Panel {
-
-	@SpringBean
-	private UserDao userDao;
 
 	private Type editType;
 	private Long userId;
@@ -23,7 +18,7 @@ public class UserDetailPanel extends Panel {
 		this.userId = userId;
 
 		UserDetailForm form = new UserDetailForm("detail-form", this.editType,
-				new AbstractEntityModel<User>(User.class, userId));
+				new AbstractEntityModel<User>(User.class, this.userId));
 		add(form);
 	}
 }
